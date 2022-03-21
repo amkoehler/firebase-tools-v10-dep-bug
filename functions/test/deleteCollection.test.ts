c5de79de548cc5a555db840e81e307770326bf60
Alex Koehler <alex@layer.team>
2022-03-21 13:14:46 -0500

    Published working test with firebase-tools v9

--- functions/test/deleteCollection.test.ts
+++ functions/test/deleteCollection.test.ts
@@ -1,20 +1,35 @@
 import { deleteCollection } from '../src';
 import * as firebaseFunctionsTest from 'firebase-functions-test';
 import { to } from 'await-to-js';
+import { getFirestore } from 'firebase-admin/firestore';
 
 const test = firebaseFunctionsTest({
   projectId: process.env.GCLOUD_PROJECT,
 });
+const db = getFirestore();
 
 describe('deleteCollection', () => {
   it('deletes collection and its subcollections', async () => {
-    // TODO set up collection and subcollection
+    const docPaths = [
+      '/parent-collection/parent-doc',
+      '/parent-collection/parent-doc/child-collection-1/child-doc-1',
+      '/parent-collection/parent-doc/child-collection-2/child-doc-2',
+    ];
+
+    for (const path of docPaths) {
+      await db.doc(path).create({ foo: 'bar' });
+    }
+
     const wrapped = test.wrap(deleteCollection);
 
-    const [err] = await to(wrapped({ collectionName: 'delete-parent' }));
+    const [err] = await to(wrapped({ collectionName: 'parent-collection' }));
 
     expect(err).toBeFalsy();
 
-    // TODO check collections
+    for (const path of docPaths) {
+      const snapshot = await db.doc(path).get();
+
+      expect(snapshot.exists).toBe(false);
+    }
   });
 });
